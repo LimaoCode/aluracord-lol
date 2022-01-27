4c05462f5804fda2ca897a41580ceeeac3d3f702
LimaoCode <contatolimaocode@gmail.com>
2022-01-27 16:27:14 -0300

    feacture/add_chat_page

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -77,11 +50,11 @@ function Subtitulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'LimaoCode';
+  const [username, setUsername] = React.useState('LimaoCode');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -103,13 +76,19 @@ export default function PaginaInicial() {
             borderRadius: '20px', padding: '32px', margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0,0,0 / 20%)',
             backgroundColor: appConfig.theme.colors.neutrals[900],
-            
+
           }}
         >
-          
+
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('Alguém submeteu o form');
+              roteamento.push('/chat');
+              // window.location.href = '/chat';
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -121,7 +100,7 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
-              {/* Photo Area */}
+            {/* Photo Area */}
             <Box
               styleSheet={{
                 display: 'flex',
@@ -159,30 +138,39 @@ export default function PaginaInicial() {
             </Box>
             {/* Photo Area */}
 
-              <TextField
-                fullWidth
-                textFieldColors={{
-                  neutral: {
-                    textColor: appConfig.theme.colors.neutrals[999],
-                    mainColor: appConfig.theme.colors.neutrals[900],
-                    mainColorHighlight: appConfig.theme.colors.neutrals[999],
-                    backgroundColor: appConfig.theme.colors.neutrals['050'],
-                  },
-                }}
-              />
-              <Button
-                type='submit'
-                label='Entrar'
-                fullWidth
-                buttonColors={{
-                  contrastColor: appConfig.theme.colors.neutrals["999"],
-                  mainColor: appConfig.theme.colors.primary[500],
-                  mainColorLight: appConfig.theme.colors.primary[600],
-                  mainColorStrong: appConfig.theme.colors.primary[900],
-                }}
-              />
-            </Box>
-            {/* Formulário */}
+            <TextField
+              value={username}
+              onChange={function (event) {
+                console.log('usuario digitou', event.target.value);
+                // Onde ta o valor?
+                const valor = event.target.value;
+                // Trocar o valor da variavel
+                // através do React e avise quem precisa
+                setUsername(valor);
+              }}
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: appConfig.theme.colors.neutrals[999],
+                  mainColor: appConfig.theme.colors.neutrals[900],
+                  mainColorHighlight: appConfig.theme.colors.neutrals[999],
+                  backgroundColor: appConfig.theme.colors.neutrals['050'],
+                },
+              }}
+            />
+            <Button
+              type='submit'
+              label='Entrar'
+              fullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["999"],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[600],
+                mainColorStrong: appConfig.theme.colors.primary[900],
+              }}
+            />
+          </Box>
+          {/* Formulário */}
 
         </Box>
       </Box>
